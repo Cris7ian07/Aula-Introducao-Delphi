75259a90f4e54045c7cad7de542d0736dad6661c
Cris7ian07 <giannini_cris_anjo@hotmail.com>
2026-04-15 22:46:58 -0300

    atividade 43

--- Componentes.dpr
+++ Componentes.dpr
@@ -7,7 +7,8 @@ uses
   UAtividade01 in 'UAtividade01.pas' {frmAtividade01},
   UAtividade27 in 'UAtividade27.pas' {frmAtividade27},
   UAtividade28 in 'UAtividade28.pas' {frmAtividade28},
-  UAtividadeM in 'UAtividadeM.pas' {frmMensagem};
+  UAtividadeM in 'UAtividadeM.pas' {frmMensagem},
+  UAtividade43 in 'UAtividade43.pas' {Form1};
 
 {$R *.res}
 
@@ -20,5 +21,6 @@ begin
   Application.CreateForm(TfrmAtividade27, frmAtividade27);
   Application.CreateForm(TfrmAtividade28, frmAtividade28);
   Application.CreateForm(TfrmMensagem, frmMensagem);
+  Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
