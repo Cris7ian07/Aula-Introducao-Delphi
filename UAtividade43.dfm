fdb91c8a8037bb93b6ee8ad14c60f09f5c1008aa
Cris7ian07 <giannini_cris_anjo@hotmail.com>
2026-04-22 21:43:00 -0300

    atividade 43

--- UAtividade43.dfm
+++ UAtividade43.dfm
@@ -7,55 +7,111 @@ object Atividade43: TAtividade43
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
-  Font.Height = -32
+  Font.Height = -19
   Font.Name = 'Segoe UI'
   Font.Style = []
-  TextHeight = 45
+  TextHeight = 25
   object lblnome: TLabel
-    Left = 72
-    Top = 32
-    Width = 90
-    Height = 38
+    Left = 8
+    Top = 8
+    Width = 60
+    Height = 25
     Caption = 'Nome :'
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
-    Font.Height = -28
+    Font.Height = -19
     Font.Name = 'Segoe UI'
     Font.Style = []
     ParentFont = False
   end
-  object lblvhora: TLabel
-    Left = 72
-    Top = 96
-    Width = 105
-    Height = 45
-    Caption = 'VHora :'
+  object lblValorHora: TLabel
+    Left = 8
+    Top = 47
+    Width = 65
+    Height = 25
+    Caption = '$ Hora :'
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
-    Font.Height = -32
+    Font.Height = -19
     Font.Name = 'Segoe UI'
     Font.Style = []
     ParentFont = False
   end
   object lblhorames: TLabel
-    Left = 72
-    Top = 168
-    Width = 153
-    Height = 45
+    Left = 8
+    Top = 90
+    Width = 89
+    Height = 25
     Caption = 'Hora m'#234's :'
   end
+  object lblFunc: TLabel
+    Left = 8
+    Top = 184
+    Width = 119
+    Height = 25
+    Caption = ' Funcion'#225'rio:  '
+  end
+  object lblSalarioBruto: TLabel
+    Left = 8
+    Top = 223
+    Width = 118
+    Height = 25
+    Caption = 'Sal'#225'rio Bruto: '
+  end
+  object lblIR: TLabel
+    Left = 8
+    Top = 254
+    Width = 40
+    Height = 25
+    Caption = '(-)IR:'
+  end
+  object lblINSS: TLabel
+    Left = 8
+    Top = 285
+    Width = 73
+    Height = 25
+    Caption = '(-) INSS: '
+  end
+  object lblSindicato: TLabel
+    Left = 8
+    Top = 316
+    Width = 107
+    Height = 25
+    Caption = '(-) Sindicato:'
+  end
+  object lblFGTS: TLabel
+    Left = 8
+    Top = 347
+    Width = 109
+    Height = 25
+    Caption = ' FGTS (11%): '
+  end
+  object lblTotalDesc: TLabel
+    Left = 8
+    Top = 378
+    Width = 162
+    Height = 25
+    Caption = 'Total de Descontos:'
+  end
+  object lblLiquido: TLabel
+    Left = 8
+    Top = 409
+    Width = 130
+    Height = 25
+    Caption = 'Sal'#225'rio L'#237'quido:'
+  end
   object edtnome: TEdit
-    Left = 208
-    Top = 32
+    Left = 74
+    Top = 8
     Width = 161
-    Height = 53
+    Height = 33
     TabOrder = 0
   end
-  object edtvhora: TEdit
-    Left = 208
-    Top = 96
+  object edtvalorhora: TEdit
+    Left = 76
+    Top = 47
     Width = 161
-    Height = 53
+    Height = 37
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
     Font.Height = -32
@@ -64,16 +120,16 @@ object Atividade43: TAtividade43
     ParentFont = False
     TabOrder = 1
   end
-  object edthorames: TEdit
-    Left = 248
-    Top = 165
+  object edthoras: TEdit
+    Left = 114
+    Top = 90
     Width = 121
-    Height = 53
+    Height = 33
     TabOrder = 2
   end
   object btncalcularsalario: TButton
-    Left = 96
-    Top = 240
+    Left = 56
+    Top = 129
     Width = 241
     Height = 49
     Caption = 'Calcular Sal'#225'rio'
--- UAtividade43.pas
+++ UAtividade43.pas
@@ -10,11 +10,19 @@ type
   TAtividade43 = class(TForm)
     lblnome: TLabel;
     edtnome: TEdit;
-    lblvhora: TLabel;
-    edtvhora: TEdit;
+    lblValorHora: TLabel;
+    edtvalorhora: TEdit;
     lblhorames: TLabel;
-    edthorames: TEdit;
+    edthoras: TEdit;
     btncalcularsalario: TButton;
+    lblFunc: TLabel;
+    lblSalarioBruto: TLabel;
+    lblIR: TLabel;
+    lblINSS: TLabel;
+    lblSindicato: TLabel;
+    lblFGTS: TLabel;
+    lblTotalDesc: TLabel;
+    lblLiquido: TLabel;
     procedure btncalcularsalarioClick(Sender: TObject);
   private
     { Private declarations }
@@ -32,32 +40,63 @@ implementation
 procedure TAtividade43.btncalcularsalarioClick(Sender: TObject);
 begin
 var
- nome, mensagem : String;
- // vhora : Real;
- // horames :Real;
-  //erro : Boolean
+    valorHora, horas, salarioBruto: Double;
 begin
- nome := edtnome.Text;
- //vhora := StrToFloatDef(edtvhora.Text, 0);
- //horames := StrToFloatDef(edthorames.Text,0);
-// erro := False;
- mensagem := 'verifique o erro:' +  sLineBreak;
- //  if (vhora = 0) and (horames = 0) then
-begin
-  //  erro := True;
-   // if vhora = 0  then
-begin
-   // if( edtvhora.Text =  FloatToStr (vhora))then
- begin
-   mensagem := 'Não digite zero,vhora' + sLineBreak;
- end;
+valorHora := StrToFloatDef(edtValorHora.Text, 0);
+ horas := StrToFloatDef(edtHoras.Text, 0);
+   salarioBruto := valorHora * horas;
 end;
-   //if horames = 0  then
- begin
-     //if( edthorames.Text =  FloatToStr (horames))then
- end;
-end;
- end;
-end;
+     function CalcularIR(salario: Double): Double;
+      begin
+      if salario <= 1900 then
+       Result := 0 else if salario <= 3500 then
+        Result := salario * 0.05 else if salario <= 7500 then
+         Result := salario * 0.10 else
+     Result := salario * 0.20;
 
-end.
+ end;
+     function CalcularINSS(salario: Double): Double;
+      begin
+      Result := salario * 0.10;
+end;
+     function CalcularSindicato(salario: Double): Double;
+      begin
+       Result := salario * 0.03;
+end;
+  function CalcularFGTS(salario: Double): Double;
+   begin
+   Result := salario * 0.11;
+end;
+  function CalcularLiquido(salario: Double): Double;
+   var ir, inss, sindicato: Double;
+   begin
+   ir := CalcularIR(salario);
+   inss := CalcularINSS(salario);
+   sindicato := CalcularSindicato(salario);
+    Result := salario - (ir + inss + sindicato);
+end;
+Tfrm43.MostrarResultado(salarioBruto: Double);
+  var
+  ir, inss, sindicato, fgts, liquido, totalDesc: Double;
+   begin
+   ir := CalcularIR(salarioBruto);
+    inss := CalcularINSS(salarioBruto);
+    sindicato := CalcularSindicato(salarioBruto);
+    fgts := CalcularFGTS(salarioBruto);
+
+    totalDesc := ir + inss + sindicato;
+     liquido := CalcularLiquido(salarioBruto);
+
+      lblFunc.Caption := 'Funcionário: ' + edtNome.Text;
+       lblSalarioBruto.Caption := 'Salário Bruto: R$ ' + FormatFloat('0.00', salarioBruto);
+
+   lblIR.Caption := '(-) IR: R$ ' + FormatFloat('0.00', ir);
+    lblINSS.Caption := '(-) INSS: R$ ' + FormatFloat('0.00', inss);
+
+    lblSindicato.Caption := '(-) Sindicato: R$ ' + FormatFloat('0.00', sindicato);
+     lblFGTS.Caption := 'FGTS (11%): R$ ' + FormatFloat('0.00', fgts);
+
+    lblTotalDesc.Caption := 'Total de Descontos: R$ ' + FormatFloat('0.00', totalDesc);
+     lblLiquido.Caption := 'Salário Líquido: R$ ' + FormatFloat('0.00', liquido);
+end;
+end;
